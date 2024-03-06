418a8d6beb3640a1df7ab570356b82bee4553fc9
amoghmasur <amoghmasur99@gmail.com>
2024-03-06 10:24:12 +0530

    generics

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -6,20 +6,30 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 
-from rest_framework import mixins
+# from rest_framework import mixins
 from rest_framework import generics
 
-class ReviewList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class ReviewList(generics.ListCreateAPIView):
+    queryset=Review.objects.all()
+    serializer_class=ReviewSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Review.objects.all()
+    serializer_class=ReviewSerializer
+
+
+# class ReviewList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 
